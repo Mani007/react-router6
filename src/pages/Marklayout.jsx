3a7c269850fafcff1d008c4814c369f03ed2b6d2
Mohnish <mohnish.a007@gmail.com>
2023-03-21 18:02:33 +0000

    dynamic page wit not found routing

--- src/pages/Marklayout.jsx
+++ src/pages/Marklayout.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 
 export default function Marklayout() {
+  const [number, setNumber] = useState(6)
   return (
     <>
     <div>
@@ -11,8 +12,9 @@ export default function Marklayout() {
         <Link to="/product/03">Product 3 </Link> <br/>
         <Link to="/product/04">Product 4 </Link> <br/>
         <Link to="/product/05">Product 5 </Link> <br/>
-        
-    </div>
+        <Link to={`/product/${number}`}>Product {number} </Link> <br/>
+        <input type="number" value={number} onChange={e => setNumber(e.target.value)} />
+          </div>
     </>
   )
 }
