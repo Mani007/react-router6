6c03b684b5022cfd0e438384cac58e58d5102c52
Mohnish <mohnish.a007@gmail.com>
2023-03-21 17:39:40 +0000

    Navigate using useNavigate hook

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 export default function Notfound() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/")
+    }, 1000)
+  }, [])
+  
   return (
-    // <div>
-    //     <h1>404 Page not found</h1>
-    // </div>
-    <Navigate/>
+    <div>
+        <h1>404 Page not found</h1>
+    </div>
+    
   )
 }
