40bd1a5860e9ce09547247f51dc42138117e0bf8
Mohnish <mohnish.a007@gmail.com>
2023-03-21 17:28:33 +0000

    Navigate component added on page not found

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 
 export default function Notfound() {
   return (
-    <div>
-        <h1>404 Page not found</h1>
-    </div>
+    // <div>
+    //     <h1>404 Page not found</h1>
+    // </div>
+    <Navigate/>
   )
 }
